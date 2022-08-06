94256876c52e0b0b07c478aa78afaae263029a08
José de Moura <mourajrn@gmail.com>
2022-08-06 14:18:39 -0300

    Include "dos" word on list

--- CarnotExcel/ExcelFunctions.cs
+++ CarnotExcel/ExcelFunctions.cs
@@ -9,7 +9,7 @@ namespace CarnotExcel
         public void AutoOpen() => IntelliSenseServer.Install();
         public void AutoClose() => IntelliSenseServer.Uninstall();
 
-        [ExcelFunction(Name = "MAIÚSCULA.CARNOT", Description = "Função para colocar a primeira letra de cada palavra em maiúsculo ignorando algumas palavras")]
+        [ExcelFunction(Name = "PRIMAIÚSCULA.CARNOT", Description = "Função para colocar a primeira letra de cada palavra em maiúsculo ignorando algumas palavras")]
         public static string capitalize([ExcelArgument(Name = "Texto", Description = "Texto para aplicar a função")] string name)
         {
             string lowerString = name.ToLower();
@@ -18,7 +18,7 @@ namespace CarnotExcel
 
             string[] wordsToIgnore = new string[]
             {
-                "a", "o", "as", "os", "de", "de", "das", "do", "da", "e", "ou", "para", "por", "no", "na", "nos", "nas"
+                "a", "o", "as", "os", "de", "de", "das", "do", "da", "e", "ou", "para", "por", "no", "na", "nos", "nas", "dos"
             };
 
             string result = "";
